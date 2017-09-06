bd06e272a4b4f1780bcbf8aeba8672d64803a960
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-09-06 13:14:57 -0600

    Added author tag to code

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,10 +1,15 @@
 package stupid.controller;
 
+/**
+ * 
+ * @author Calvin.Berrett
+ *
+ */
 public class StupidController
 {
 
 		public void start()
 		{
-			System.out.println("Somebody once told me the world was gonna roll me, I ain't the sharpest tool in the shed");
+			System.out.println("Somebody once told me the world was gonna roll me, I ain't the sharpest tool in the shed.");
 		}
 }
