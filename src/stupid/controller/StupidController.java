bdb2c4008301eb13204a6ffbdfa8f46a9389a378
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-09-06 12:55:33 -0600

    Initial commit

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,10 @@
+package stupid.controller;
+
+public class StupidController
+{
+
+		public void start()
+		{
+			System.out.println("Somebody once told me the world was gonna roll me, I ain't the sharpest tool in the shed");
+		}
+}
