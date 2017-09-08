2b36402ba70e20f46caa0cdc0df5a9544fd531f6
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2017-09-08 11:10:02 -0600

    Added a scanner so the computer can type back

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,8 +1,11 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
+
 /**
  * 
- * @author Calvin.Berrett
+ * @author cber6181
  *
  */
 public class StupidController
@@ -10,6 +13,17 @@ public class StupidController
 
 		public void start()
 		{
-			System.out.println("Somebody once told me the world was gonna roll me, I ain't the sharpest tool in the shed.");
+			System.out.println("Somebody once told me the world was gonna roll me, I ain't the sharpest tool in the shed. She was looking kind of dumb with her finger and her thumb in the shape of an L on her forehead");
+			
+			testScanner();
+		}
+		
+		private void testScanner
+		{
+			Scanner firstScanner; 
+			firstScanner = new Scanner(System.in);
+			System.out.println("What's your favorite food");
+			String answer = firstScanner.next();
+			System.out.println("Oh cool, you like" + answer);
 		}
 }
